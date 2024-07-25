6c376995ab3d7d1bedc66551ca757a3548dbb04c
Mohamed Taha Hassen <mt337848@gmail.com>
2024-07-25 16:29:53 +0300

    create note model

---
+++ lib/model/notes_model.dart
@@ -0,0 +1,16 @@
+class NotesModel {
+  final String title;
+
+  final String suptitle;
+
+  final String date;
+
+  final int color;
+
+  NotesModel(
+      {required this.title,
+      required this.suptitle,
+      required this.date,
+      required this.color,
+      });
+}
